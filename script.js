1de36ca3a686ee14f6f3068e04470ca129135415
Manojvittedi <153159811+Manojvittedi@users.noreply.github.com>
2026-04-02 11:33:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let select = document.getElementById("colorSelect");
+
+function removeColor(){
+	let index = select.selectedIndex;
+	if(index !=-1){
+		select.remove(index);
+	}
+}
